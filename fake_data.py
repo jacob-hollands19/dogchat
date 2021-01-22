151fbaf6d28c69983f7b481888c5f21709619de4
jacob-hollands19 <73252388+jacob-hollands19@users.noreply.github.com>
2021-01-21 16:49:40 -0800

    Worked on adding data

--- fake_data.py
+++ fake_data.py
@@ -26,17 +26,59 @@ def get_dog_by_handle(handle):
     return None
 
 posts = [
-    ("melba", "I'm so excited to move to California!"),
-    ("melba", "Great game of fetch today with my Dad, Paul"),
-    ("chucky", "Took a great 8 hour nap today, then guarded the household"),
-    ("melba", "Peanut butter is my favorite snack!"),
-    ("rose", "Today I stole food from a blind dog.")
+    {
+        "Postee": "melba",
+        "Post": "I'm so excited to move to California!",
+        "LikedBy": "chucky, rose, tyler",
+        "Likes": "3",
+        "Reposts": "5"
+    },
+    {
+        "Postee": "melba",
+        "Post": "Great game of fetch today with my Dad, Paul",
+        "LikedBy": "chucky, rose, tyler",
+        "Likes": "3",
+        "Reposts": "1"
+        
+    },
+    {
+        "Postee": "chucky",
+        "Post": "Took a great 8 hour nap today, then guarded the household",
+        "LikedBy": "melba, tyler",
+        "Likes": "2",
+        "Reposts": "19"
+    },
+    {
+        "Postee": "melba",
+        "Post": "Peanut butter is my favorite snack!",
+        "LikedBy": "chucky, rose",
+        "Likes": "3",
+        "Reposts": "5"
+    },
+    {
+        "Postee": "rose",
+        "Post": "Today I stole food from a blind dog.",
+        "LikedBy": "chucky",
+        "Likes": "1",
+        "Reposts": "2"
+    },
+    {
+        "Postee": "rose",
+        "Post": "Today my owner fed me ramen.",
+        "Likes": "",
+        "Likes": "0",
+        "Reposts": "3"
+    }
 ]
-
 
 def get_posts_by_handle(handle):
     results = []
+    temp = []
     for post in posts:
-        if post[0] == handle:
-            results.append(post)
+        if post['Postee'] == handle:
+            temp.append(post['Postee'])
+            temp.append(post['Post'])
+            results.append(temp)
+            temp = []
     return results
+
